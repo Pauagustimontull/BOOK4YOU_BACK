c6894daa541ebfbeb4d881106b7236258e83d137
Pauagustimontull <pauagustimontull@gmail.com>
2023-11-08 21:49:45 +0100

    server

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ const oracledb = require('oracledb');
 db={
     user:'23_24_DAM2_CUCOTUCSI',
     password:'cucorulo',
-    //connectString:'oracle.ilerna.com:1521'
-    connectString:'192.168.3.26:1521'
+    connectString:'oracle.ilerna.com:1521'
+    //connectString:'192.168.3.26:1521'
 }
 async function open(sql, binds, autoCommit){
     let con = await oracledb.getConnection(db);
