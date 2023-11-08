377602c904d95747819b2fbd4d50c198e3c29f99
Pauagustimontull <pauagustimontull@gmail.com>
2023-11-08 20:07:22 +0100

    Subida

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,8 @@
 const {Router} = require('express');
 const router = Router();
 const DB = require('../config/config');
-
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
 router.get('/', (req, res)=> {
     res.status(200).json({
@@ -42,13 +43,125 @@ router.get('/usuarios',async (req, res)=> {
 
 });
 
+router.post('/login', async (req, res) => {
+    const { EMAIL, PASSWORD } = req.body;
+    const sql = "SELECT * FROM USUARIO WHERE EMAIL = :EMAIL"; // Busca al usuario por email
+    const binds = {
+        EMAIL
+    };
+    
+    try {
+        const result = await DB.Open(sql, binds, false);
+        
+        if (result.rows.length === 1) {
+            const user = result.rows[0];
+            const storedPasswordHash = user[3]; // Supongo que la contraseña está hasheada en la posición 3
+
+            // Hashea la contraseña proporcionada y compárala con la almacenada
+            const providedPasswordHash = hashPassword(PASSWORD);
+            
+            if (storedPasswordHash === providedPasswordHash) {
+                // Las contraseñas coinciden, el inicio de sesión es exitoso
+                res.json({ user: user });
+            } else {
+                // Las contraseñas no coinciden
+                res.status(401).json({ message: 'Contraseña incorrecta', user: user });
+            }
+        } else {
+            // No se encontró ningún usuario con el correo electrónico proporcionado
+            res.status(404).json({ message: 'Usuario no encontrado', user: user });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al realizar la operación de login' });
+    }
+});
 
 
 
+router.post('/usuarios/inside', async (req, res) => {
+    const { NAME, EMAIL, PASSWORD, DNII, SURNAME } = req.body;
+    const sql = `
+        INSERT INTO USUARIO (NAME, EMAIL, PASSWORD, DNII, SURNAME)
+        VALUES (:NAME, :EMAIL, :PASSWORD, :DNII, :SURNAME)`;
+    const binds = {
+        NAME,
+        EMAIL,
+        PASSWORD,
+        DNII,
+        SURNAME
+    };
+
+    let result;
+    try {
+        result = await DB.Open(sql, binds, true);
+        res.json({ message: 'Usuario insertado con éxito' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al insertar el usuario' });
+    }
+});
+router.post('/apartamentos', async (req, res) => {
+    const { ID } = req.body;
+    const apartamentos = [];
+    const sql = "SELECT * FROM APARTMENT WHERE ID_OWNER = :ID"; // Utiliza :ID como marcador de posición
+    const binds = {
+        ID
+    };
+
+    try {
+        const result = await DB.Open(sql, binds, true);
+        const apartmentList = result.rows.map(apartamento => {
+            return {
+                "ID": apartamento[0],  
+                "IMAGE": apartamento[1],
+                "NAME": apartamento[2], 
+                "PRICE": apartamento[3],
+                "PLACE": apartamento[4],  
+                "AVALIABLE": apartamento[5],
+                "CITY": apartamento[6],
+                "ID_OWNER": apartamento[7]
+            };
+        });
+
+        res.json({ apartments: apartmentList });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al buscar apartamentos' });
+    }
+});
 
 
+router.post('/apartamentos/inside', async (req, res) => {
+    const { ID_OWNER,IMAGE, NAME, PRICE, PLACE, AVALIABLE, CITY } = req.body;
+    const sql = `
+        INSERT INTO APARTMENT (ID_OWNER, IMAGE, NAME, PRICE, PLACE, AVALIABLE, CITY)
+        VALUES ( :ID_OWNER, :IMAGE, :NAME, :PRICE, :PLACE, :AVALIABLE, :CITY)
+    `;
+    const binds = {
+        ID_OWNER,
+        IMAGE,
+        NAME,
+        PRICE,
+        PLACE,
+        AVALIABLE,
+        CITY
+    };
 
+    try {
+        const result = await DB.Open(sql, binds, true);
+        res.json({ message: 'Apartamento insertado con éxito' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al insertar el apartamento' });
+    }
+});
 
+function hashPassword(password) {
+    const hash = crypto.createHash('sha256');
+    const hashedPassword = hash.update(password, 'utf8').digest('hex');
+    return hashedPassword;
+}
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
-
 const router = require('../routes/routes');
 
 app.set('port', 2020);
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors()); // Añadido para habilitar CORS
 
 app.use(router);
 
-app.listen(app.get('port'),()=>{
+app.listen(app.get('port'), () => {
     console.log("Live at Port 2020");
 });
